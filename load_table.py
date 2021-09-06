aa6c81d24ae4d3716c4c69319d6459ab8dbd4031
Marcos Huerta <sirmarcos@gmail.com>
2021-09-06 19:15:34 -0400

    umap utility function

--- load_table.py
+++ load_table.py
@@ -5,7 +5,11 @@
 import re
 import sqlite3
 from tqdm import tqdm
+from umap import UMAP
+import numpy as np
+import pandas as pd
 
+from sqlalchemy import create_engine, text
 #model =
 dictionary = 'words.txt'
 pattern = r"^[a-z][a-z-]*[a-z]$"
@@ -39,6 +43,8 @@ def write_glove_to_sql(model="glove.840B.300d.txt"):
 def shrink_glove_model_file(model='glove.840B.300d.txt'):
     '''This should shrink the 2GB+ glove text file to 270MB by matching with the hunspell dictionary'''
     words = set()
+
+    #read in hunspell based dictionary
     with open(dictionary, "r") as f:
         for line in f:
             if re.match(pattern, line):
@@ -46,9 +52,36 @@ def shrink_glove_model_file(model='glove.840B.300d.txt'):
     with open('new_model.txt', 'wt') as gf:
         with open(model, "r") as f:
             i = 0
-            for line in (f):
+            for line in tqdm(f):
                 i = i + 1
                 tokens = line.split(" ")
+                #only write lines that match the Hunspell dictionary\
                 if tokens[0] in words:
-                    #only write lines that match the Hunspell dictionary
                     gf.write(line)
+
+
+def create_umap_table_sql(model='new_model.txt',
+                          random_state=42,
+                          n_neighbors=15,
+                          **kwargs):
+    umap_model = UMAP(n_neighbors=n_neighbors, random_state=random_state)
+
+    #reading vectors
+    print("reading vectors and names from text file")
+    names = np.genfromtxt('new_model.txt',
+                          delimiter=' ',
+                          usecols=0,
+                          dtype='str')
+    #grab everything but the words
+    usecols = list(range(1, 301))
+
+    vectors = np.genfromtxt(model, delimiter=' ', usecols=usecols)
+    print("creating umap")
+    transformed_data = umap_model.fit_transform(vectors, **kwargs)
+    umap_df = pd.DataFrame(transformed_data, index=names,
+                           columns=['A', 'B']).reset_index()
+    dbc = create_engine("sqlite:///dat.db")
+    print('uploading to sql')
+    umap_df.rename({
+        'index': 'word'
+    }, axis=1).to_sql('umap', con=dbc, index=False, if_exists='replace')
