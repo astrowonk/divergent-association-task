a60b1ca866c13ea9c7ccbc3864aafcf6be1c4ae6
Marcos Huerta <sirmarcos@gmail.com>
2021-09-06 20:12:23 -0400

    dash app tweaks

--- dash_app.py
+++ dash_app.py
@@ -1,27 +1,31 @@
 from dash import Dash, Input, Output, State, html, dcc, callback, no_update
 import dash_bootstrap_components as dbc
-import plotly.express as px
 
 app = Dash('myapp', external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-input_div = html.Div(
-    dcc.Input(id='words-input',
-              type='text',
-              placeholder='Enter words here, separated by spaces.',
-              persistence=True,
-              style={
-                  'width': '100%',
-                  'height': '60px',
-                  'font-size': '20px'
-              }))
+input_div = html.Div(dcc.Input(
+    id='words-input',
+    type='text',
+    placeholder='Enter words here, separated by spaces.',
+    persistence=True,
+    style={
+        'width': '100%',
+        'height': '60px',
+        'font-size': '20px'
+    }),
+                     style={'width': '100%'})
 score = html.H4(id='score')
-graph = html.Div(id='graph')
+graph = html.Div(id='graph', style={'width': '90%'})
 
 from dat import Model
 m = Model()
 m.disable_minimum = True
 
-app.layout = dbc.Container([dbc.Row([input_div, score, graph])])
+app.layout = dbc.Container([
+    dbc.Row([input_div]),
+    dbc.Row(score),
+    dbc.Row(graph),
+])
 
 
 @app.callback(Output('score', 'children'), Output('graph', 'children'),
@@ -36,10 +40,9 @@ def update_score(words):
         return no_update
     score = m.dat(word_list)
     fig = m.plot_words(word_list)
-    fig.update_layout(yaxis_range=[-6.155959, 5.337202],
-                      xaxis_range=[-5.572776, 5.194516])
+
     return f"Average Semantic Distance: {score.round(2)}", dcc.Graph(
-        id='figure',
+        id='umap-figure',
         figure=fig,
         style={'height': '80vh'},
         config={'displayModeBar': False})
